2e931f5830f46a862787acdf3fd3926acc9ff33b
filip7557 <61994637+filip7557@users.noreply.github.com>
2023-07-01 17:34:56 +0200

    Stopped saving empty TODOs

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,8 +6,8 @@ const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
         setInputText(e.target.value);
     };
     const submitTodoHandler = (e) => {
+        e.preventDefault();
         if(inputText !== "") {
-            e.preventDefault();
             setTodos([
                 ...todos, {text: inputText, completed: false, id: Math.random() * 1000}
             ]);
